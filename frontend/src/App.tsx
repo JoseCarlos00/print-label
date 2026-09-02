2120f49d452dee21d224428066ce7ff9b69f374e
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2026-09-02 10:01:05 -0600

    Config Initial Fronted

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
   return (
-    <>
-      <h1>App.tsx</h1>
-    </>
-  )
+		<>
+			<h1 className='text-3xl font-bold underline text-amber-900'>Hello world!</h1>
+		</>
+	);
 }
 
 export default App
