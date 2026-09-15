c6b9fb4365617bf181ad1f111d44c08f5fbddd7d
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2026-09-15 15:19:51 -0600

    Feat: Install  shadcn

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [
@@ -9,6 +13,11 @@ export default defineConfig({
     tailwindcss(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     proxy: {
       '/api': 'http://192.168.15.189:8001',
